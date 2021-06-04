1b577dc45f4837e6d96f6d8135cd617bfcd46e9c
tony-go <gorez.tony@gmail.com>
2021-06-04 17:28:44 +0200

    chore: change editorconfig and format files

--- test/test.js
+++ test/test.js
@@ -4,18 +4,18 @@ import test from "tape";
 import sizeSatisfies from "../index.js";
 
 test("invalid operator always return false", (tape) => {
-    tape.strictEqual(sizeSatisfies("!! 45KB", "45KB"), false);
-    tape.end();
+  tape.strictEqual(sizeSatisfies("!! 45KB", "45KB"), false);
+  tape.end();
 });
 
 test("assert sizeSatisfies", (tape) => {
-    tape.strictEqual(sizeSatisfies(">= 45KB", "20MB"), true);
-    tape.strictEqual(sizeSatisfies("<= 45KB", "10B"), true);
-    tape.strictEqual(sizeSatisfies("= 45KB", "45KB"), true);
-    tape.strictEqual(sizeSatisfies("= 45KB", "46KB"), false);
-    tape.strictEqual(sizeSatisfies("= 45KB", 2000), false);
-    tape.strictEqual(sizeSatisfies("> 45KB", "46KB"), true);
-    tape.strictEqual(sizeSatisfies("> 45KB", "45KB"), false);
-    tape.strictEqual(sizeSatisfies("< 45KB", "44KB"), true);
-    tape.end();
+  tape.strictEqual(sizeSatisfies(">= 45KB", "20MB"), true);
+  tape.strictEqual(sizeSatisfies("<= 45KB", "10B"), true);
+  tape.strictEqual(sizeSatisfies("= 45KB", "45KB"), true);
+  tape.strictEqual(sizeSatisfies("= 45KB", "46KB"), false);
+  tape.strictEqual(sizeSatisfies("= 45KB", 2000), false);
+  tape.strictEqual(sizeSatisfies("> 45KB", "46KB"), true);
+  tape.strictEqual(sizeSatisfies("> 45KB", "45KB"), false);
+  tape.strictEqual(sizeSatisfies("< 45KB", "44KB"), true);
+  tape.end();
 });
